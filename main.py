9085891709db9436d730766f23eeb7d03fdc831a
James Langley <jwblangley@gmail.com>
2021-12-28 09:51:54 +0000

    Refactor svg generation to generator

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import torchvision.transforms.functional as TF
 
 from median_cut import median_cut, paint_lines, paint_centroids
 
+PAINT_THICKNESS = 10
+
 
 def edge_detect(img):
     filter = torch.tensor([[1, 2, 1]], dtype=torch.float32)
@@ -23,8 +25,27 @@ def edge_detect(img):
     return img
 
 
+def generate_svg_lines(img, centroids):
+    yield f"""<svg version="1.1"
+    width="{img.size(2)}" height="{img.size(1)}"
+    xmlns="http://www.w3.org/2000/svg">\n\n"""
+
+    for centroid in centroids:
+        _, _, bbox = centroid
+        x1, y1, x2, y2 = bbox
+        col = img[:, y1:y2, x1:x2].flatten(1).mean(dim=1) * 255
+
+        cx = x1 + (x2 - x1) // 2
+        cy = y1 + (y2 - y1) // 2
+        r = min(x2 - x1, y2 - y1) // 2
+        col = col.int()
+
+        yield f'\t<circle cx="{cx}" cy="{cy}" r="{r}" fill="rgb({col[0]},{col[1]},{col[2]})" />\n'
+
+    yield "\n</svg>\n"
+
+
 if __name__ == "__main__":
-    PAINT_THICKNESS = 10
 
     img = Image.open("picture.jpg")
     img = TF.to_tensor(img)
@@ -39,28 +60,6 @@ if __name__ == "__main__":
     plt.imshow(mc_img)
     plt.show()
 
-    svg_header = f"""<svg version="1.1"
-    width="{img.size(2)}" height="{img.size(1)}"
-    xmlns="http://www.w3.org/2000/svg">\n\n"""
-
-    svg_footer = "\n</svg>\n"
-
-    svg_content = svg_header
-
-    for centroid in centroids:
-        _, _, bbox = centroid
-        x1, y1, x2, y2 = bbox
-        col = img[:, y1:y2, x1:x2].flatten(1).mean(dim=1) * 255
-
-        cx = x1 + (x2 - x1) // 2
-        cy = y1 + (y2 - y1) // 2
-        r = min(x2 - x1, y2 - y1) // 2
-        col = col.int()
-
-        svg = f'\t<circle cx="{cx}" cy="{cy}" r="{r}" fill="rgb({col[0]},{col[1]},{col[2]})" />\n'
-        svg_content += svg
-
-    svg_content += svg_footer
-
     with open("out.svg", "w") as outfile:
-        outfile.write(svg_content)
+        for svg_line in generate_svg_lines(img, centroids):
+            outfile.write(svg_line)
